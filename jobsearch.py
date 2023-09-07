99192c0bb3e9fef22ad431c4c6f69f78049aade0
qiqi47 <c756738098@126.com>
2023-09-07 01:17:08 -0500

    sep 7

--- jobsearch.py
+++ jobsearch.py
@@ -5,7 +5,7 @@ def search_on_job_sites(keywords):
     job_sites = {
         # in Indeed ，fromage=1 represents jobs published in last one day
         "Indeed": "https://www.indeed.com/jobs?q={}&fromage=1",
-        # in LinkedIn中，r86400 represents jobs published in last one day
+        # in LinkedIn，r86400 represents jobs published in last one day
         "LinkedIn": "https://www.linkedin.com/jobs/search/?keywords={}&f_TPR=r86400",
         # in Glassdoor ，fromAge=1d represents jobs published in last one day
         "Glassdoor": "https://www.glassdoor.com/Job/jobs.htm?sc.keyword={}&fromAge=1d"
